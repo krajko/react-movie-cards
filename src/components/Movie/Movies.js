683d8ebf4ba0eb3d72e3c887af8bdd45e13c23b6
krajko <nikolakrajinovic99@gmail.com>
2022-01-21 12:08:17 +0100

    init

---
+++ src/components/Movie/Movies.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+
+import MovieList from './MovieList';
+import MovieService from '../../services/MovieService';
+
+const Movies = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    setMovies(MovieService.getMovies());
+  }, []);
+
+  return (
+    <div className="container-fluid" style={{ marginLeft: '-15px' }}>
+      <div className="d-flex flex-row">
+        <div className="col-sm-12">
+          <MovieList movies={movies} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Movies;
